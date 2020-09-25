9713169ec3995b6c3fc3c22f8acd3c633b70d111
Luke0195 <lucas201273@gmail.com>
2020-09-24 21:05:43 -0300

    feat: add credencias do banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,17 @@
 // Credencias com a base de dados.
 
 module.exports ={
-    dialect: 'postgres',
+    dialect: 'mysql2',
+    host: 'localhost',
+    user:'root',
+    password:'',
+    database: 'sqlnode',
+    define:{
+      timestamps: true,
+      underscored: true,
+
+    },
 }
+
+// timestamps -> vai criar os campos created_at e update_at.
+// underscored-> As tabelas e colunas terão o espaçamento com colunas.
